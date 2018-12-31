264e15504611367e31248d6e162c0d4bcde16d9e
James Davey <james.t.davey@gmail.com>
2018-12-30 22:42:39 -0800

    function that takes a sorted array and number, and checks if the number is an average of any two numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
-const areThereDuplicates = (...arr) => {
-  const lookup = {};
-  for (let item of arr) {
-    lookup[item] = (lookup[item] || 0) + 1;
-    if (lookup[item] > 1) {
+const averagePair = (arr, num) => {
+  if (arr.length === 0) {
+    return false;
+  }
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    if ((arr[left] + arr[right]) / 2 === num) {
       return true;
+    } else if ((arr[left] + arr[right]) / 2 > num) {
+      right --;
+    } else if ((arr[left] + arr[right]) / 2 < num) {
+      left ++;
     }
   }
   return false;
 }
-
-areThereDuplicates(1, 2, 3, 3);
