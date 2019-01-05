fd807ef05c9dd2e0cc7b4fa3995a1a748e7068de
James Davey <james.t.davey@gmail.com>
2019-01-05 11:51:02 -0800

    add linearSearch function with same functionality as indexOf

--- assets/js/script.js
+++ assets/js/script.js
@@ -422,3 +422,19 @@ let obj = {
 
 console.log('stringifyNumbers');
 console.log(stringifyNumbers(obj));
+
+// linearSearch
+
+const linearSearch = (arr, val) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+console.log('linearSearch');
+console.log(linearSearch([10, 15, 20, 25, 30], 15)); // 1
+console.log(linearSearch([1, 2, 3, 4, 5], 6)); // -2
+console.log(linearSearch([100], 100)); // 0
