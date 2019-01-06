e95afaa9744a4118e8a65dccd8c259cb849d638f
James Davey <james.t.davey@gmail.com>
2019-01-06 14:55:48 -0800

    fix bubble sort typo to subtract i instead of 1, and subtract another 1 to avoid extra compare

--- assets/js/script.js
+++ assets/js/script.js
@@ -480,7 +480,7 @@ console.log(naiveStringSearch('whatisthiswhatamiwhatareyoudoing', 'what')); // 3
 
 const bubbleSort = arr => {
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1; j++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
