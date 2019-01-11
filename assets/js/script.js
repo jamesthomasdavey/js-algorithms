081186b87cdbe8e6fe2fc2500ab6604a9f618abb
James Davey <james.t.davey@gmail.com>
2019-01-10 21:52:14 -0800

    rewrite selectionSort function for practice

--- assets/js/script.js
+++ assets/js/script.js
@@ -495,15 +495,15 @@ console.log(bubbleSort([55, 21, 42, 93, 6, 18, 22, 14]));
 // selectionSort
 
 const selectionSort = arr => {
-  for (let i = 0; i < arr.length; i++) {
-    let index = i;
+  for (let i = 0; i < arr.length - 1; i++) {
+    let min = i;
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[index]) {
-        index = j;
+      if (arr[j] < arr[min]) {
+        min = j;
       }
     }
-    if (index !== i) {
-      [arr[i], arr[index]] = [arr[index], arr[i]];
+    if (min !== i) {
+      [arr[i], arr[min]] = [arr[min], arr[i]];
     }
   }
   return arr;
